37602085fc1f390c9bc689f255916a4f9e34770f
Matthias Kellnhofer <m.kellnhofer@dracoon.com>
2020-09-11 14:01:34 +0200

    Set min API version for new crypto algorithms to 4.24.0
    See: #SDKJAVA-127

--- src/main/java/com/dracoon/sdk/internal/DracoonConstants.java
+++ src/main/java/com/dracoon/sdk/internal/DracoonConstants.java
@@ -15,7 +15,7 @@ public interface DracoonConstants {
     String API_MIN_VERSION = "4.12.0";
     String API_MIN_PASSWORD_POLICIES = "4.13.0";
     String API_MIN_S3_DIRECT_UPLOAD = "4.15.0";
-    String API_MIN_NEW_CRYPTO_ALGOS = "4.99.0";
+    String API_MIN_NEW_CRYPTO_ALGOS = "4.24.0";
 
     String API_TIME_ZONE = "UTC";
     String API_DATE_FORMAT = "yyyy-MM-dd";
