2d95f4991e0a63219576d0b2891094e38821ed22
Matthias Kellnhofer <m.kellnhofer@dracoon.com>
2022-02-18 14:18:33 +0100

    Split up DracoonAuth tests
    See: #SDKJAVA-177

--- src/test/java/com/dracoon/sdk/DracoonAuthTest.java
+++ src/test/java/com/dracoon/sdk/DracoonAuthTest.java
@@ -1,5 +1,6 @@
 package com.dracoon.sdk;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,51 +13,73 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DracoonAuthTest {
 
+    private DracoonAuth mAuth;
+
     // --- Tests of configuration for Authorization Code Mode ---
 
     @Nested
     class CreateAuthConfigAuthorizationCodeModeTests {
 
+        private final String CLIENT_ID = "xGcYjy2R";
+        private final String CLIENT_SECRET = "OCz812WX";
+        private final String AUTHORIZATION_CODE = "whdLferi";
+
+        @BeforeEach
+        void setup() {
+             mAuth = new DracoonAuth(CLIENT_ID, CLIENT_SECRET, AUTHORIZATION_CODE);
+        }
+
         @Test
-        void testAuthConfigCorrect() {
-            String clientId = "xGcYjy2R";
-            String clientSecret = "OCz812WX";
-            String authorizationCode = "whdLferi";
-
-            DracoonAuth auth = new DracoonAuth(clientId, clientSecret, authorizationCode);
-
-            assertEquals(DracoonAuth.Mode.AUTHORIZATION_CODE, auth.getMode(),
+        void testModeCorrect() {
+            assertEquals(DracoonAuth.Mode.AUTHORIZATION_CODE, mAuth.getMode(),
                     "Authorization mode is incorrect!");
-            assertEquals(clientId, auth.getClientId(),
-                    "Client ID does not match!");
-            assertEquals(clientSecret, auth.getClientSecret(),
-                    "Client secret does not match!");
-            assertEquals(authorizationCode, auth.getAuthorizationCode(),
-                    "Authorization code does not match!");
+        }
 
-            assertNull(auth.getAccessToken(), "Access token is not null!");
-            assertNull(auth.getRefreshToken(), "Refresh token is not null!");
+        @Test
+        void testClientIdCorrect() {
+            assertEquals(CLIENT_ID, mAuth.getClientId(), "Client ID does not match!");
+        }
+
+        @Test
+        void testClientSecretCorrect() {
+            assertEquals(CLIENT_SECRET, mAuth.getClientSecret(), "Client secret does not match!");
+        }
+
+        @Test
+        void testAuthorizationCodeCorrect() {
+            assertEquals(AUTHORIZATION_CODE, mAuth.getAuthorizationCode(),
+                    "Authorization code does not match!");
+        }
+
+        @Test
+        void testAccessTokenCorrect() {
+            assertNull(mAuth.getAccessToken(), "Access token is not null!");
+        }
+
+        @Test
+        void testRefreshTokenCorrect() {
+            assertNull(mAuth.getRefreshToken(), "Refresh token is not null!");
         }
 
         @ParameterizedTest
         @NullAndEmptySource
         void testClientIdValidation(String clientId) {
             assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(clientId,
-                    "OCz812WX", "whdLferi"));
+                    CLIENT_SECRET, AUTHORIZATION_CODE));
         }
 
         @ParameterizedTest
         @NullAndEmptySource
         void testClientSecretValidation(String clientSecret) {
-            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth("xGcYjy2R",
-                    clientSecret, "whdLferi"));
+            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(CLIENT_ID,
+                    clientSecret, AUTHORIZATION_CODE));
         }
 
         @ParameterizedTest
         @NullAndEmptySource
         void testAuthorizationCodeValidation(String authorizationCode) {
-            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth("xGcYjy2R",
-                    "OCz812WX", authorizationCode));
+            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(CLIENT_ID,
+                    CLIENT_SECRET, authorizationCode));
         }
 
     }
@@ -66,21 +89,42 @@ class DracoonAuthTest {
     @Nested
     class CreateAuthConfigAccessTokenModeTests {
 
+        private final String ACCESS_TOKEN = "jZokGKAx05dwm59X";
+
+        @BeforeEach
+        void setup() {
+            mAuth = new DracoonAuth(ACCESS_TOKEN);
+        }
+
         @Test
-        void testAuthConfigCorrect() {
-            String accessToken = "jZokGKAx05dwm59X";
-
-            DracoonAuth auth = new DracoonAuth(accessToken);
-
-            assertEquals(DracoonAuth.Mode.ACCESS_TOKEN, auth.getMode(),
+        void testModeCorrect() {
+            assertEquals(DracoonAuth.Mode.ACCESS_TOKEN, mAuth.getMode(),
                     "Authorization mode is incorrect!");
-            assertEquals(accessToken, auth.getAccessToken(),
-                    "Access token does not match!");
+        }
 
-            assertNull(auth.getClientId(), "Client ID is not null!");
-            assertNull(auth.getClientSecret(), "Client secret is not null!");
-            assertNull(auth.getAuthorizationCode(), "Authorization code is not null!");
-            assertNull(auth.getRefreshToken(), "Refresh token is not null!");
+        @Test
+        void testAccessTokenCorrect() {
+            assertEquals(ACCESS_TOKEN, mAuth.getAccessToken(), "Access token does not match!");
+        }
+
+        @Test
+        void testClientIdCorrect() {
+            assertNull(mAuth.getClientId(), "Client ID is not null!");
+        }
+
+        @Test
+        void testClientSecretCorrect() {
+            assertNull(mAuth.getClientSecret(), "Client secret is not null!");
+        }
+
+        @Test
+        void testAuthorizationCodeCorrect() {
+            assertNull(mAuth.getAuthorizationCode(), "Authorization code is not null!");
+        }
+
+        @Test
+        void testRefreshTokenCorrect() {
+            assertNull(mAuth.getRefreshToken(), "Refresh token is not null!");
         }
 
         @ParameterizedTest
@@ -96,55 +140,73 @@ class DracoonAuthTest {
     @Nested
     class CreateAuthConfigAccessRefreshTokenModeTests {
 
+        private final String CLIENT_ID = "xGcYjy2R";
+        private final String CLIENT_SECRET = "OCz812WX";
+        private final String ACCESS_TOKEN = "jZokGKAx05dwm59X";
+        private final String REFRESH_TOKEN = "hvOIDqcRrP5ppdyt";
+
+        @BeforeEach
+        void setup() {
+            mAuth = new DracoonAuth(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN);
+        }
+
         @Test
-        void testAuthConfigCorrect() {
-            String clientId = "xGcYjy2R";
-            String clientSecret = "OCz812WX";
-            String accessToken = "jZokGKAx05dwm59X";
-            String refreshToken = "hvOIDqcRrP5ppdyt";
-
-            DracoonAuth auth = new DracoonAuth(clientId, clientSecret, accessToken, refreshToken);
-
-            assertEquals(DracoonAuth.Mode.ACCESS_REFRESH_TOKEN, auth.getMode(),
+        void testModeCorrect() {
+            assertEquals(DracoonAuth.Mode.ACCESS_REFRESH_TOKEN, mAuth.getMode(),
                     "Authorization mode is incorrect!");
-            assertEquals(clientId, auth.getClientId(),
-                    "Client ID does not match!");
-            assertEquals(clientSecret, auth.getClientSecret(),
-                    "Client secret does not match!");
-            assertEquals(accessToken, auth.getAccessToken(),
-                    "Access token does not match!");
-            assertEquals(refreshToken, auth.getRefreshToken(),
-                    "Refresh token does not match!");
+        }
 
-            assertNull(auth.getAuthorizationCode(), "Authorization code is not null!");
+        @Test
+        void testClientIdCorrect() {
+            assertEquals(CLIENT_ID, mAuth.getClientId(), "Client ID does not match!");
+        }
+
+        @Test
+        void testClientSecretCorrect() {
+            assertEquals(CLIENT_SECRET, mAuth.getClientSecret(), "Client secret does not match!");
+        }
+
+        @Test
+        void testAccessTokenCorrect() {
+            assertEquals(ACCESS_TOKEN, mAuth.getAccessToken(), "Access token does not match!");
+        }
+
+        @Test
+        void testRefreshTokenCorrect() {
+            assertEquals(REFRESH_TOKEN, mAuth.getRefreshToken(), "Refresh token does not match!");
+        }
+
+        @Test
+        void testAuthorizationCodeCorrect() {
+            assertNull(mAuth.getAuthorizationCode(), "Authorization code is not null!");
         }
 
         @ParameterizedTest
         @NullAndEmptySource
         void testClientIdValidation(String clientId) {
             assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(clientId,
-                    "OCz812WX", "jZokGKAx05dwm59X", "hvOIDqcRrP5ppdyt"));
+                    CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN));
         }
 
         @ParameterizedTest
         @NullAndEmptySource
         void testClientSecretValidation(String clientSecret) {
-            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth("xGcYjy2R",
-                    clientSecret, "jZokGKAx05dwm59X", "hvOIDqcRrP5ppdyt"));
+            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(CLIENT_ID,
+                    clientSecret, ACCESS_TOKEN, REFRESH_TOKEN));
         }
 
         @ParameterizedTest
         @NullAndEmptySource
         void testAccessTokenValidation(String accessToken) {
-            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth("xGcYjy2R",
-                    "OCz812WX", accessToken, "hvOIDqcRrP5ppdyt"));
+            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(CLIENT_ID,
+                    CLIENT_SECRET, accessToken, REFRESH_TOKEN));
         }
 
         @ParameterizedTest
         @EmptySource
         void testRefreshTokenValidation(String refreshToken) {
-            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth("xGcYjy2R",
-                    "OCz812WX", "jZokGKAx05dwm59X", refreshToken));
+            assertThrows(IllegalArgumentException.class, () -> new DracoonAuth(CLIENT_ID,
+                    CLIENT_SECRET, ACCESS_TOKEN, refreshToken));
         }
 
     }
